31a8bb6067c5d3a0c8271fb6ace3ce375c538c0f
Sidoine De Wispelaere <sidoine@sidoine.net>
2018-08-20 09:10:32 +0200

    Package

--- index.lua
+++ index.lua
@@ -1,4 +1,4 @@
-local __exports = LibStub:NewLibrary("AceDB-3.0", 10000)
+local __exports = LibStub:NewLibrary("AceDB-3.0", 10100)
 if not __exports then return end
 local kpairs = pairs
 local lib = {
